f5d9514b09fb33f39765cb522373aa415feeab13
Nonso <chinonsoiwuchukwu7@gmail.com>
2023-03-11 14:31:51 +0100

    First commit

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,6 +1,29 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = 'mario'
+    const [name, setName]= useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge(40);
+    };
+
+    // const handleClickAgain = (name) => {
+    //     console.log('hellooo '  + name);
+    // }
+
     return (  
-        <h1>HOME PAGE</h1>
+        <div className="home">
+             <h1>HOME PAGE</h1>
+             <p> { name } is { age } years old.</p>
+             <button onClick={handleClick}>Click me</button>
+             {/* <button onClick= {() => handleClickAgain('John')}>Click to see some Shit</button> */}
+
+        </div>
+       
+       
     );
 }
  
