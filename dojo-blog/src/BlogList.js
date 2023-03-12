975d43b461ffc41975c76acbb5e7a34b92203823
Nonso <chinonsoiwuchukwu7@gmail.com>
2023-03-12 20:39:51 +0100

    commit

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title,}) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
@@ -9,11 +9,9 @@ const BlogList = ({blogs, title, handleDelete}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
     ); 
 }
-
 export default BlogList
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,28 +3,29 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
     
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() =>{
+            fetch('http://localhost:8000/blogs')
             .then(res => {
-                return res.json()
+                return res.json();
             })
             .then(data => {
-                console.log(data);
-                setBlogs(data)
-            })
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000);
     }, []);
 
-    return (  
-        <div className="home">
-           {blogs && <BlogList blogs={blogs} title ="All Blogs!" handleDelete={handleDelete}/>}
-        </div>   
+    return (
+          <div className="home">
+            { isPending && <div>Loading...<div/> }
+            {blogs && <BlogList blogs={blogs} title ="All Blogs!" />}
+        </div>
     );
 }
- 
+         
+
 export default Home;
