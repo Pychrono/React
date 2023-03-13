5848151e8fb8f3a355a891a8b5b3888172e84e6c
Nonso <chinonsoiwuchukwu7@gmail.com>
2023-03-13 17:36:39 +0100

    another commit

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,30 +2,15 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true)
-    
-
-    useEffect(() => {
-        setTimeout(() =>{
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            });
-        }, 1000);
-    }, []);
-
+    const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  
     return (
-          <div className="home">
-            { isPending && <div>Loading...<div/> }
-            {blogs && <BlogList blogs={blogs} title ="All Blogs!" />}
-        </div>
+      <div className="space">
+        { error && <div>{ error }</div> }
+        { isPending && <div>Loading...</div> }
+        { blogs && <BlogList blogs={blogs} /> }
+      </div>
     );
-}
-         
+  }
 
 export default Home;
