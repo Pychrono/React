59289ae13918d38a4dd3a80d1a578a000b23acf9
Nonso <chinonsoiwuchukwu7@gmail.com>
2023-03-14 22:27:46 +0100

    new nav

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>THe Dojo Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </nav>
-     );
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+  );
 }
  
 export default Navbar;
